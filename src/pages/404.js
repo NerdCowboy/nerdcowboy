ff8cab0494189def03219e0595858741be2c14cd
Brent Larson <taulars@gmail.com>
2018-03-18 19:08:06 -0700

    Add basic 404 page
    - Add basic 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import u from '../global-styles/utilities.module.scss'
+import btn from '../components/common/c-button.module.scss'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div styleName="u.page-wrapper u.reading-width">
+    <h1>This page is a goner</h1>
+    <p>Real sorry, but there's nothing to see in these parts.</p>
+    <p>The page you tried to visit may have been moved or deleted.</p>
+    <p>
+      You can use the top navigation to try to find what you were looking for.
+    </p>
+    <p>
+      Or if you know the page you're trying to visit should be here, let me
+      know.
+    </p>
+    <a styleName="btn.button btn.button--primary" href="/contact">
+      Report broken page
+    </a>
   </div>
 )
 
