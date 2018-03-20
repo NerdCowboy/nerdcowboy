35fecbc6a808f0e8b696522ab87f01b92ad4e719
Brent Larson <taulars@gmail.com>
2018-03-19 20:04:18 -0700

    Turn on gatsby plugin offline
    - Uncomment gatsby-plugin-offline as I had it off for debugging
    reasons and forgot to turn it back on
    - Remove custom options as I don’t believe I need them

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,3 @@
-const rootDir = './'
-
-const offlineOptions = {
-  staticFileGlobs: [
-    `${rootDir}**/*.{js,woff2}`,
-    `${rootDir}index.html`,
-    // `${rootDir}favicons/manifest.json`,
-    `${rootDir}offline-plugin-app-shell-fallback/index.html`,
-  ],
-  stripPrefix: rootDir,
-  navigateFallback: `/offline-plugin-app-shell-fallback/index.html`,
-  // Only match URLs without extensions.
-  // So example.com/about/ will pass but
-  // example.com/cheeseburger.jpg will not.
-  // We only want the service worker to handle our "clean"
-  // URLs and not any files hosted on the site.
-  navigateFallbackWhitelist: [/^.*(?!\.\w?$)/],
-  cacheId: `gatsby-plugin-offline`,
-  // Do cache bust JS URLs until can figure out how to make Webpack's
-  // URLs truely content-addressed.
-  dontCacheBustUrlsMatching: /(.\w{8}.woff2)/, // |-\w{20}.js)/,
-  runtimeCaching: [
-    {
-      // Add runtime caching of images.
-      urlPattern: /\.(?:png|jpg|jpeg|webp|svg|gif|tiff)$/,
-      handler: `fastest`,
-    },
-  ],
-  skipWaiting: false,
-}
-
 module.exports = {
   siteMetadata: {
     title: `Nerd Cowboy - UI/UX Designer & Frontend Developer`,
@@ -92,6 +61,6 @@ module.exports = {
         ],
       },
     },
-    // `gatsby-plugin-offline`, // needs to be after manifest
+    `gatsby-plugin-offline`, // needs to be after manifest
   ],
 }
