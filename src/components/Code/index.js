48dd4a0530e479794fc075efdb853cb31abce8a4
NerdCowboy <taulars@gmail.com>
2020-10-16 19:59:29 -0700

    Add copy button to code

--- src/components/Code/index.js
+++ src/components/Code/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 import theme from 'prism-react-renderer/themes/nightOwl'
@@ -11,6 +11,26 @@ export const Code = ({
   'react-live': reactLive,
   ...props
 }) => {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const copyToClipboard = (str) => {
+    const el = document.createElement('textarea')
+    el.value = str
+    el.setAttribute('readonly', '')
+    el.style.position = 'absolute'
+    el.style.left = '-9999px'
+    document.body.appendChild(el)
+    el.select()
+    document.execCommand('copy')
+    document.body.removeChild(el)
+  }
+
+  const copyButton = (string) => {
+    copyToClipboard(string)
+    setIsCopied(true)
+    setTimeout(() => setIsCopied(false), 3000)
+  }
+
   if (reactLive) {
     return (
       <div className={styles.container}>
@@ -33,6 +53,13 @@ export const Code = ({
       >
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre className={className} style={style}>
+            <button
+              type="button"
+              onClick={() => copyButton(codeString)}
+              className={styles.copyButton}
+            >
+              {!isCopied ? 'Copy' : 'Copied'}
+            </button>
             {tokens.map((line, i) => (
               <div {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
